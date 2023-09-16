44ce515d6bce8038088fed57a6d92e9e8c8a7e81
alisa-po <alisa.popova21@gmail.com>
2023-09-17 01:28:20 +0200

    added testes for featture 1, scenario2

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,5 +1,13 @@
-const EventList = () => {
-  return <div id="event-list"></div>;
+import Event from "./Event";
+
+const EventList = ({ events }) => {
+  return (
+    <ul id="event-list">
+      {events
+        ? events.map((event) => <Event key={event.id} event={event} />)
+        : null}
+    </ul>
+  );
 };
 
 export default EventList;
