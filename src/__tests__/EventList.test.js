55438efa6eba9e06cdba84b7490ef7cc96d297f9
alisa-po <alisa.popova21@gmail.com>
2023-09-17 08:52:17 +0200

    added tests feature1, scenario 3

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,15 +1,18 @@
 import { render } from "@testing-library/react";
 import EventList from "../components/EventList";
+import { getEvents } from "../api";
 
 describe("<EventList /> component", () => {
   test('has an element with "list" role', () => {
     const EventListComponent = render(<EventList />);
     expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
   });
-  test("renders correct number of events", () => {
-    const EventListComponent = render(
-      <EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />
+
+  test("renders correct number of events", async () => {
+    const allEvents = await getEvents();
+    const EventListComponent = render(<EventList events={allEvents} />); // Define EventListComponent here
+    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(
+      allEvents.length
     );
-    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
   });
 });
