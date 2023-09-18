498833ff94997c306f9ce732c3ae60ec850ce1c6
alisa-po <alisa.popova21@gmail.com>
2023-09-18 03:19:39 +0200

    added feature 2 and 3 tests

---
+++ src/components/NumberOfEvents.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const NumberOfEvents = ({
+  eventNumber,
+  onEventNumberChange,
+  setErrorAlert,
+}) => {
+  const handleInputChanged = (value) => {
+    const numberValue = parseInt(value); // Convert the input value to a number
+    if (!isNaN(numberValue)) {
+      onEventNumberChange(numberValue);
+    } else {
+      onEventNumberChange(32);
+    }
+    let errorText;
+    if (isNaN(value) || value <= 0) {
+      errorText = "You must enter a positive number to continue.";
+    } else {
+      errorText = "";
+    }
+    setErrorAlert(errorText);
+  };
+
+  return (
+    <div id="number-of-events">
+      <input
+        onFocus={() => {
+          onEventNumberChange("");
+        }}
+        type="text"
+        className="textbox"
+        placeholder="Enter a number"
+        value={eventNumber}
+        onChange={(e) => handleInputChanged(e.target.value)}
+      />
+    </div>
+  );
+};
+export default NumberOfEvents;
