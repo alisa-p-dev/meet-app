eb39bd4626f88637696b1f5747830e87a14b91a1
alisa-po <alisa.popova21@gmail.com>
2023-09-28 02:31:44 +0200

    added offline mode

--- src/api.js
+++ src/api.js
@@ -79,6 +79,13 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    return events ? JSON.parse(events) : [];
+  }
+
+  const token = await getAccessToken();
+
   if (token) {
     removeQuery();
     const url =
@@ -88,11 +95,8 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
-      NProgress.done();
       localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
   }
-
-  const token = await getAccessToken();
 };
