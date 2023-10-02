cfb64c051af67c40a47a575876e5be2531c820ae
alisa-po <alisa.popova21@gmail.com>
2023-10-02 16:41:00 +0200

    added ScatterChart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
 import NumberOfEvents from "./components/NumberOfEvents";
+import CityEventsChart from "./components/CityEventsChart";
 import { useEffect, useState } from "react";
 import { extractLocations, getEvents } from "./api";
 import { InfoAlert, ErrorAlert, WarningAlert } from "./components/Alert";
@@ -56,6 +57,7 @@ const App = () => {
         setCurrentCity={setCurrentCity}
         setInfoAlert={setInfoAlert}
       ></CitySearch>
+      <CityEventsChart allLocations={allLocations} events={events} />
       <EventList events={events}></EventList>
     </div>
   );
